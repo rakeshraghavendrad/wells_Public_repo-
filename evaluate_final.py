831ad08ebcbf180485a8d539ab6018bd2790528a
rakeshraghavendrad <71114677+rakeshraghavendrad@users.noreply.github.com>
2025-04-05 13:42:11 +0530

    Update evaluate_final.py

--- evaluate_final.py
+++ evaluate_final.py
@@ -151,7 +151,7 @@ score_df = merged_df[['method_name', 'score_gained', 'max_score', 'remarks']].co
 # Add other metadata columns using .loc to avoid SettingWithCopyWarning
 score_df.loc[:, 'UserEmail'] = user_email
 score_df.loc[:, 'attempt_id'] = attempt_id
-score_df.loc[:, 'timestamp'] = datetime.now(ZoneInfo("Asia/Kolkata"))
+score_df.loc[:, 'timestamp'] = datetime.now(ZoneInfo("Asia/Kolkata")).replace(tzinfo=None)
 score_df.loc[:, 'project'] = project
 
 # Final column order
